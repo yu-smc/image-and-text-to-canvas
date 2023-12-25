793740c201bae93b2fc1df2bc35008534a72f3c2
Yu Takada <yu.smc.roundabout@gmail.com>
2023-12-25 23:55:14 +0900

    renderingまで

--- build/image-and-text-to-canvas.js
+++ build/image-and-text-to-canvas.js
@@ -1,15 +1,37 @@
+import html2canvas from "./lib/html2canvas.esm.js";
+
 export class ImageTextToCanvas {
   sourceImageElement = null;
+  sourceImageAspectRatio = null;
   wrapperElement = null;
-  outputImageElement = null;
+  textContainerElement = null;
+  textElement = null;
 
-  constructor({ wrapperElement, outputImageElement, generateOptions }) {
+  constructor({ wrapperElement }) {
     this.wrapperElement = wrapperElement;
-    this.outputImageElement = outputImageElement;
+
+    this.init();
   }
 
-  setup() {
+  init() {
+    //initialize necessary styles and calculations
+    if (getComputedStyle(this.wrapperElement).position === "static") {
+      this.wrapperElement.style.position = "relative";
+    }
+  }
+
+  setup({
+    textColor,
+    textWidth,
+    textHeight,
+    hideOverflowText,
+    horizontalTextAlignment,
+    verticalTextAlignment,
+  }) {
     this.sourceImageElement = this.wrapperElement.querySelector("img");
+    this.sourceImageAspectRatio =
+      this.sourceImageElement.naturalHeight /
+      this.sourceImageElement.naturalWidth;
 
     if (!this.sourceImageElement) {
       console.error(
@@ -17,5 +39,66 @@ export class ImageTextToCanvas {
       );
       return;
     }
+
+    //create dom elements for text input
+    this.textContainerElement = document.createElement("div");
+    this.textContainerElement.id = "itc-text-container";
+
+    this.textElement = document.createElement("div");
+    this.textElement.id = "itc-text";
+    this.textElement.contentEditable = true;
+
+    //set text elements style
+    this.textContainerElement.style.position = "absolute";
+    this.textContainerElement.style.top = "0";
+    this.textContainerElement.style.right = "0";
+    this.textContainerElement.style.bottom = "0";
+    this.textContainerElement.style.left = "0";
+    this.textContainerElement.style.margin = "auto";
+    this.textContainerElement.style.width = textWidth;
+    this.textContainerElement.style.height = textHeight;
+
+    if (hideOverflowText) {
+      this.textContainerElement.style.overflow = "hidden";
+    }
+
+    this.textElement.style.width = "100%";
+    this.textElement.style.height = "100%";
+    this.textElement.style.color = textColor;
+
+    if (horizontalTextAlignment === "center") {
+      this.textElement.style.textAlign = "center";
+    }
+
+    // if (verticalTextAlignment === "center") {
+    //   this.textElement.style.display = 'flex'
+    //   this.textElement.style.alignItems = 'center'
+    // }
+
+    this.textContainerElement.appendChild(this.textElement);
+    this.wrapperElement.appendChild(this.textContainerElement);
+  }
+
+  async generate({ outputImageWidthPx, quality }) {
+    return new Promise(async (resolve, reject) => {
+      if (typeof outputImageWidthPx !== "number") {
+        console.alert('"outputImageWidthPx must be number"');
+        reject();
+      }
+
+      const outputCanvas = await html2canvas(this.wrapperElement, {
+        backgroundColor: null,
+        scale: 3,
+        // allowTaint: true,
+      });
+
+      resolve(outputCanvas.toDataURL("image/jpeg", quality || 0.9));
+
+      // const canvasWidth = outputImageWidthPx;
+      // const canvasHeight = canvasWidth * this.sourceImageAspectRatio;
+
+      // canvas.width = canvasWidth;
+      // canvas.height = canvasHeight;
+    });
   }
 }
--- demo/js/image-and-text-to-canvas.js
+++ demo/js/image-and-text-to-canvas.js
@@ -1,23 +1,104 @@
-class ImageTextToCanvas {
-  sourceImageElement = null;
-  wrapperElement = null
-  outputImageElement = null
+import html2canvas from "./lib/html2canvas.esm.js";
 
-  constructor({wrapperElement, outputImageElement, generateOptions}) {
-    this.wrapperElement = wrapperElement
-    this.outputImageElement = outputImageElement
+export class ImageTextToCanvas {
+  sourceImageElement = null;
+  sourceImageAspectRatio = null;
+  wrapperElement = null;
+  textContainerElement = null;
+  textElement = null;
+
+  constructor({ wrapperElement }) {
+    this.wrapperElement = wrapperElement;
+
+    this.init();
   }
 
-  setup() {
-    this.sourceImageElement = this.wrapperElement.querySelector('img')
+  init() {
+    //initialize necessary styles and calculations
+    if (getComputedStyle(this.wrapperElement).position === "static") {
+      this.wrapperElement.style.position = "relative";
+    }
+  }
+
+  setup({
+    textColor,
+    textWidth,
+    textHeight,
+    hideOverflowText,
+    horizontalTextAlignment,
+    verticalTextAlignment,
+  }) {
+    this.sourceImageElement = this.wrapperElement.querySelector("img");
+    this.sourceImageAspectRatio =
+      this.sourceImageElement.naturalHeight /
+      this.sourceImageElement.naturalWidth;
 
     if (!this.sourceImageElement) {
-      console.error('There must be 1 img element inside the wrapper element, but 0 was found')
-      return
+      console.error(
+        "There must be 1 img element inside the wrapper element, but 0 was found"
+      );
+      return;
     }
 
+    //create dom elements for text input
+    this.textContainerElement = document.createElement("div");
+    this.textContainerElement.id = "itc-text-container";
 
+    this.textElement = document.createElement("div");
+    this.textElement.id = "itc-text";
+    this.textElement.contentEditable = true;
+
+    //set text elements style
+    this.textContainerElement.style.position = "absolute";
+    this.textContainerElement.style.top = "0";
+    this.textContainerElement.style.right = "0";
+    this.textContainerElement.style.bottom = "0";
+    this.textContainerElement.style.left = "0";
+    this.textContainerElement.style.margin = "auto";
+    this.textContainerElement.style.width = textWidth;
+    this.textContainerElement.style.height = textHeight;
+
+    if (hideOverflowText) {
+      this.textContainerElement.style.overflow = "hidden";
+    }
+
+    this.textElement.style.width = "100%";
+    this.textElement.style.height = "100%";
+    this.textElement.style.color = textColor;
+
+    if (horizontalTextAlignment === "center") {
+      this.textElement.style.textAlign = "center";
+    }
+
+    // if (verticalTextAlignment === "center") {
+    //   this.textElement.style.display = 'flex'
+    //   this.textElement.style.alignItems = 'center'
+    // }
+
+    this.textContainerElement.appendChild(this.textElement);
+    this.wrapperElement.appendChild(this.textContainerElement);
+  }
+
+  async generate({ outputImageWidthPx, quality }) {
+    return new Promise(async (resolve, reject) => {
+      if (typeof outputImageWidthPx !== "number") {
+        console.alert('"outputImageWidthPx must be number"');
+        reject();
+      }
+
+      const outputCanvas = await html2canvas(this.wrapperElement, {
+        backgroundColor: null,
+        scale: 3,
+        // allowTaint: true,
+      });
+
+      resolve(outputCanvas.toDataURL("image/jpeg", quality || 0.9));
+
+      // const canvasWidth = outputImageWidthPx;
+      // const canvasHeight = canvasWidth * this.sourceImageAspectRatio;
+
+      // canvas.width = canvasWidth;
+      // canvas.height = canvasHeight;
+    });
   }
 }
-
-export { ImageTextToCanvas }
